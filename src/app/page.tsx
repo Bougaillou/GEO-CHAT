c5c736d8eb4082bd29bca49ef134ad3dc53ac3fc
Bougaillou <bougaillou.ibrahim@gmail.com>
2025-06-27 16:48:57 +0100

    side bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { useState } from "react";
 import { assets } from "@/assets/assets"
+import SideBar from "@/components/SideBar";
 
 export default function Home() {
 
@@ -13,6 +14,10 @@ export default function Home() {
     <>
       <div>
         <div className="flex h-screen">
+          <SideBar
+            expand={expand}
+            setExpand={setExpand}
+          />
           <div className="flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative">
             <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
               <Image onClick={() => (expand ? setExpand(false) : setExpand(true))} className="rotate-180" src={assets.menu_icon} alt="menu_icon" />

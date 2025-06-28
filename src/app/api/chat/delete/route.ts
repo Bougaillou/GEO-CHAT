3910a4dc1b4b9f5128b8484a22a7ae7387c1cf42
Bougaillou <bougaillou.ibrahim@gmail.com>
2025-06-28 21:00:02 +0100

    API CHAT DELETE

---
+++ src/app/api/chat/delete/route.ts
@@ -0,0 +1,24 @@
+import connectDB from "@/config/db";
+import Chat from "@/models/Chat";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        const { userId } = getAuth(req)
+        const {chatId} = await req.json()
+
+        if (!userId) {
+            return new Response(JSON.stringify({ succes: false, error: "Unauthorized" }), { status: 401 });
+        }
+
+        await connectDB()
+        await Chat.deleteOne({ _id: chatId, userId })
+
+        return new Response(JSON.stringify({ success: true, message: "Chat Delete" }), { status: 200 })
+
+    } catch (error) {
+        const err = error as Error
+        return new Response(JSON.stringify({ success: false, error: err.message }), { status: 400 })
+    }
+}

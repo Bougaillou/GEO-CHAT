480a94372241b3f8b1c69e02e15adb1c1d5be7ed
Bougaillou <bougaillou.ibrahim@gmail.com>
2025-06-28 20:52:15 +0100

    API CHAT GET

---
+++ src/app/api/chat/get/route.ts
@@ -0,0 +1,22 @@
+import connectDB from "@/config/db";
+import Chat from "@/models/Chat";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+    try {
+        const { userId } = getAuth(req)
+        if (!userId) {
+            return new Response(JSON.stringify({ succes:false, error: "Unauthorized" }), { status: 401 });
+        }
+
+        await connectDB()
+        const data = await Chat.find({userId})
+
+        return new Response(JSON.stringify({ success: true, data }), { status: 200 })
+
+    } catch (error) {
+        const err = error as Error
+        return new Response(JSON.stringify({ success: false, error: err.message }), { status: 400 })
+    }
+}

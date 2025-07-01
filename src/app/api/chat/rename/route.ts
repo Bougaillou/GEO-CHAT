d25a802c3130a3984439f496f65df3926f0442a8
Bougaillou <bougaillou.ibrahim@gmail.com>
2025-07-01 10:49:07 +0100

    modification 2

--- src/app/api/chat/rename/route.ts
+++ src/app/api/chat/rename/route.ts
@@ -1,6 +1,6 @@
 // 'use server'
 import connectDB from "@/config/db";
-import Chat from "@/models/chat";
+import Chat from "@/models/Chat";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextRequest } from "next/server";
 
